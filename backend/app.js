416a4a84b6cf234c47c6f7209586d2f6ec588e0a
andher1802 <Andres@BRN401C-9RD2.artsci.uc.edu>
2018-07-23 17:20:42 -0400

    back version 07232018

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,13 @@
-var express = require('express');
-var app = express.use();
+const express = require('express');
+var app = express();
+
+app.use((req, res, next) => {
+console.log('first middleware');
+next();
+});
+
+app.use((req, res, next) => {
+    res.send('Hello from back');
+});
+
+module.exports = app;
