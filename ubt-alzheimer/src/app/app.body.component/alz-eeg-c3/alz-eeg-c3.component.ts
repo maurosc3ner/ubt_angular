12df8b7207ebc9adec7782e0895cb0afaa1bcb93
Esteban Correa <maurosc3ner@gmail.com>
2018-10-09 12:00:24 -0400

    ENT100 and ENTROPY RE added

--- ubt-alzheimer/src/app/app.body.component/alz-eeg-c3/alz-eeg-c3.component.ts
+++ ubt-alzheimer/src/app/app.body.component/alz-eeg-c3/alz-eeg-c3.component.ts
@@ -9,22 +9,27 @@ import * as c3 from 'c3';
 export class AlzEegC3Component implements OnInit {
   @Input() EEG_Status_eeg;
   @Input() current_data = null;
-  chart;
-  data1=[];
-  data1_x;
-  firstDraw: boolean;
+  chartPLETH;
+  chartENT100;
+  chartENTROPYRE;
+  dataPLETH=[];
+  dataENT100=[];
+  dataENTROPYRE=[]
+  dataPLETH_x;
+  fdPLETH: boolean;
+  fdENT100: boolean;
+  fdENTROPYRE: boolean;
   constructor() { }
 
   ngOnInit() {
-    
-
-    this.firstDraw=false;
-    this.data1 = ['PLETH', 0,0,0,0,0];
-    this.chart = c3.generate({
-      bindto: '#chart',
+    //PLETH
+    this.fdPLETH=false;
+    this.dataPLETH = ['PLETH', 0,0,0,0,0];
+    this.chartPLETH = c3.generate({
+      bindto: '#chartPLETH',
       data: {
         columns: [
-          this.data1
+          this.dataPLETH
         ],
         type: 'spline'
       },
@@ -39,37 +44,131 @@ export class AlzEegC3Component implements OnInit {
         }
       }
     });
-    
+    //ENT_100
+    this.fdENT100=false;
+    this.dataENT100 = ['ENT100', 0,0,0,0,0];
+    this.chartENT100 = c3.generate({
+      bindto: '#chartENT100',
+      data: {
+        columns: [
+          this.dataENT100
+        ],
+        type: 'spline'
+      },
+      
+      color: {
+        pattern: ['#ffffff']
+      },
+      axis: {
+        y: {
+            max: 100,
+            min: -100
+        }
+      }
+    });
+    //ENTROPY_RE
+    this.fdENTROPYRE=false;
+    this.dataENTROPYRE = ['ENTROPY_RE', 0,0,0,0,0];
+    this.chartENTROPYRE = c3.generate({
+      bindto: '#chartENTROPYRE',
+      data: {
+        columns: [
+          this.dataENTROPYRE
+        ],
+        type: 'spline'
+      },
+      
+      color: {
+        pattern: ['#ffffff']
+      },
+      axis: {
+        y: {
+            max: 100,
+            min: -100
+        }
+      }
+    });
     
   
 
   }
 
   ngOnChanges() {
-    console.log('EC3-ngOnChanges',this.data1.length);
-    if (this.current_data['channels'][0]["label"]=='PLETH'){
-      if (this.data1.length<=500 && this.firstDraw==false){
-        for ( let i = 0; i < this.current_data['channels'][0]["data"].length; i += 1 ) {
-          this.data1.push(this.current_data['channels'][0]["data"][i]);
-        }
-          
-        this.data1.push(this.getRndInteger(1,10));
-      }else{
-        this.data1=[];
-        this.data1.push('PLETH');
-        for ( let i = 0; i < this.current_data['channels'][0]["data"].length; i += 1 ) {
-          this.data1.push(this.current_data['channels'][0]["data"][i]);
-        }
-        this.firstDraw=true;
-      }
+    
+    // this.current_data["channels"].forEach((currentChannel,index, array)=>{
+    //   console.log('EC3-channels traverse',currentChannel,index);
+    // });
+    if (!this.isEmpty(this.current_data) && this.current_data["debug"]["command"]=="request_channels"){
+      //console.log('EC3-ngOnChanges',this.current_data);
+      this.current_data["channels"].forEach((currentChannel,index, array)=>{
+        console.log('EC3-ngOnChanges',currentChannel["label"]);
+        if (currentChannel["label"]=='PLETH'){
+          if (this.dataPLETH.length<=300 && this.fdPLETH==false){
+            for ( let i = 0; i < currentChannel["data"].length; i += 1 ) {
+              this.dataPLETH.push(currentChannel["data"][i]);
+            } 
+          }else{
+            this.dataPLETH=[];
+            this.dataPLETH.push('PLETH');
+            for ( let i = 0; i < currentChannel["data"].length; i += 1 ) {
+              this.dataPLETH.push(currentChannel["data"][i]);
+            }
+            this.fdPLETH=true;
+          } //end esle
+          this.chartPLETH.flow({
+            columns: [
+              this.dataPLETH
+            ],
+            duration: 250,
+            length: currentChannel["data"].length
+          });
+        } //end fi PLETH
+        else if (currentChannel["label"]=='ENT_100'){
+          if (this.dataENT100.length<=500 && this.fdPLETH==false){
+            for ( let i = 0; i < 100; i += 1 ) {
+              this.dataENT100.push(currentChannel["data"][i]);
+            } 
+          }else{
+            this.dataENT100=[];
+            this.dataENT100.push('ENT100');
+            for ( let i = 0; i < 100; i += 1 ) {
+              this.dataENT100.push(currentChannel["data"][i]);
+            }
+            this.fdENT100=true;
+          } //end esle
+          this.chartENT100.flow({
+            columns: [
+              this.dataENT100
+            ],
+            duration: 250,
+            length: 100
+          });
+        } //end fi ENT100
+        else if (currentChannel["label"]=='ENTROPY RE'){
+          if (this.dataENTROPYRE.length<=5 && this.fdPLETH==false){
+            for ( let i = 0; i < currentChannel["data"].length; i += 1 ) {
+              this.dataENTROPYRE.push(currentChannel["data"][i]);
+            } 
+          }else{
+            this.dataENTROPYRE=[];
+            this.dataENTROPYRE.push('ENTROPY_RE');
+            for ( let i = 0; i < currentChannel["data"].length; i += 1 ) {
+              this.dataENTROPYRE.push(currentChannel["data"][i]);
+            }
+            this.fdENTROPYRE=true;
+          } //end esle
+          this.chartENTROPYRE.flow({
+            columns: [
+              this.dataENTROPYRE
+            ],
+            duration: 250,
+            length: currentChannel["data"].length
+          });
+        } //end fi ENTROPY RE
 
-      this.chart.flow({
-        columns: [
-          this.data1
-        ],
-        duration: 250,
-        length: this.current_data['channels'][0]["data"].length
+
       });
+
     }
 
     
@@ -82,36 +181,31 @@ export class AlzEegC3Component implements OnInit {
   onAddClick(event){
     
 
-console.log('EC3-onAddClick',this.data1,this.data1.length);
-    if (this.data1.length<=12 && this.firstDraw==false){
-      this.data1.push(this.getRndInteger(1,10));
-      this.data1.push(this.getRndInteger(1,10));
+// console.log('EC3-onAddClick',this.dataPLETH,this.dataPLETH.length);
+    if (this.dataPLETH.length<=12 && this.fdPLETH==false){
+      this.dataPLETH.push(this.getRndInteger(1,10));
+      this.dataPLETH.push(this.getRndInteger(1,10));
     }else{
-      this.data1=['data1' ,this.getRndInteger(1,10),this.getRndInteger(1,10)];
-      this.firstDraw=true;
+      this.dataPLETH=['PLETH' ,this.getRndInteger(1,10),this.getRndInteger(1,10)];
+      this.fdPLETH=true;
     }
-
-    
-      // this.data1.push(this.getRndInteger(1,5));
-      // this.data1.push(this.getRndInteger(4,9));
-      console.log('EC3-onAddClick',this.data1,this.data1.length);
-      // this.chart.flush();
-
-      this.chart.flow({
+      console.log('EC3-onAddClick',this.dataPLETH,this.dataPLETH.length);
+      this.chartPLETH.flow({
         columns: [
-          this.data1
+          this.dataPLETH
         ],
         duration: 0,
         length:2
       });
-    
-    
-
   }
 
   getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
   } 
+
+  isEmpty(obj) {
+    return Object.keys(obj).every(k => !Object.keys(obj[k]).length)
+  }
 
 
 
